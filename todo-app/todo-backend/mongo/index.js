9eb5d969bfc920fa6170c8b30d8ee8a3b0cb9a6d
Pavel <imran.pavel@gmail.com>
2025-10-13 21:13:44 +0300

    12.9

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -5,7 +5,10 @@ const { MONGO_URL } = require('../util/config')
 if (MONGO_URL && !mongoose.connection.readyState) {
   mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('DB Connected.'))
-  .catch(() => console.log('DB Connection Failed!'))
+  .catch((e) => {
+    console.log('e', e)
+    console.log('DB Connection Failed!')
+  })
 }
 
 
